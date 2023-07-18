1145e96ea4db20cc0daf63dbe3ff5717c5ed5546
crazycaveman <crazycaveman@users.noreply.github.com>
2023-07-17 22:20:04 -0400

    Add import_csv.py and transactions.py
    Simple CSV file reader and import transactions to EveryDollar
      Defaults to using transactions.csv if no file args
      Modify as needed for other CSV layouts
    Base transactions.csv for testing/example purposes

---
+++ import_csv.py
@@ -0,0 +1,64 @@
+"""
+Load CSV file(s) and add transactions to EveryDollar
+"""
+
+from everydollar_api import EveryDollarAPI
+from datetime import datetime
+import csv
+import sys
+import time
+
+try:
+    from creds import username, password
+except ImportError:
+    import sys
+    print("Please create a creds.py file with the variables username and password defined with your everydollar credentials")
+    print("")
+    print("example creds.py: ")
+    print("username = \"my username\"")
+    print("password = \"my password\"")
+    sys.exit()
+
+# Initialize variables
+# Modify dateFmt to match format of dates in your CSV
+#  Year - %Y is 4 digit, %y is 2 digit
+#  Month, day - %m, %d
+#  Hour - %H in 24 hour clock, %I is 12 hour clock
+#  Minute - %M
+#  Second - %S
+dateFmt = "%m/%d/%Y" #03/24/2023
+# Column names
+HEADERS = {"date": "Post Date",
+           "exp": "Debit",
+           "inc": "Credit",
+           "desc": "Description"
+          }
+filenames = ['transactions.csv']
+totalTx = 0
+
+api = EveryDollarAPI()
+api.login(username, password)
+
+if len(sys.argv) > 1:
+    filenames = sys.argv[1:]
+
+print("Sending transactions to EveryDollar")
+for file in filenames:
+    with open(file, newline='') as csvfile:
+        filereader = csv.DictReader(csvfile, delimiter=',')
+        TxCount = 0
+        
+        for row in filereader:
+            info = {"Amt": float(row[HEADERS['inc']]) if row[HEADERS['inc']] != "" else float(f"-{row[HEADERS['exp']]}"),
+                    "Date": datetime.strptime(row[HEADERS['date']], dateFmt),
+                    "Desc": row[HEADERS['desc']]
+                   }
+            api.add_transaction(info["Date"], info["Desc"], abs(info["Amt"]), "expense" if info["Amt"] < 0 else "income")
+            time.sleep(1.0)
+            TxCount += 1
+        
+        totalTx += TxCount
+        if len(sys.argv) > 2:
+            print(f"Processed {TxCount} transactions from {file}")
+
+print(f"Added {totalTx} transactions. Now for the fun part!")
